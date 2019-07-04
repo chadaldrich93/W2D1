e27ba2c4ef4ce53a52216a878cd18eecfa5b5cd2
Chad Aldrich <chadaldrich93@gmail.com>
2019-07-04 00:27:58 -0400

    added sort string array

--- SortStringArray.java
+++ SortStringArray.java
@@ -15,11 +15,13 @@ public class SortStringArray {
         strings.add("zebra");
         strings.add("lemonade");
         strings.add("bananas");
-        List<String> ordered, reverseOrdered, firstLetterOrdered;
+        List<String> ordered, reverseOrdered, firstLetterOrdered, eFirstOrdered, eFirstOrdered2;
         ordered = sortStringsByLength(strings);
         reverseOrdered = reverseSortStringsByLength(strings);
         firstLetterOrdered = sortByFirstChar(strings);
-        for (String s: firstLetterOrdered)
+        eFirstOrdered = sortStringsWithEFirst(strings);
+        eFirstOrdered2 = sortStringsWithEFirst(strings);
+        for (String s: eFirstOrdered2)
         	System.out.println(s);
 	}
 	
@@ -44,10 +46,28 @@ public class SortStringArray {
 		return orderedByFirstChar;
 	}
 	
-	public static List<String> sortStringsByEFirst(ArrayList<String> strings) {
-		List<String> orderedByEFirst = strings.stream()
-				                              .sorted( (s1,s2) -> if (s1.charAt(0) == 'e'))
+	public static List<String> sortStringsWithEFirst(ArrayList<String> strings) {
+		List<String> stringsWithEFirst = strings.stream()
+				                              .sorted( (s1,s2) -> { if (s1.contains("e"))
+				                            		                   return -1;
+				                                                     return 1;
+				                                                  }
+				                              )
 				                              .collect(Collectors.toList());
-		return orderedByEFirst;
+		return stringsWithEFirst;
+	}
+	
+	public static List<String> cleanerSortStringsWithEFirst(ArrayList<String> strings) {
+		List<String> stringsWithEFirst = strings.stream()
+				                              .sorted( (s1,s2) -> eIsInString(s1, s2))
+				                              .collect(Collectors.toList());
+		return stringsWithEFirst;
+	}
+	
+	private static int eIsInString(String s1, String s2){
+		if (s1.contains("e")) {
+			return -1;
+		}
+		return 1;
 	}
 }
